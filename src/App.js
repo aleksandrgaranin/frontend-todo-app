98a81b0eea3b5642e1e7550e33f0cdcd97b3638f
aleksandGG <aleksandrgaranin89@gmail.com>
2021-01-13 23:24:12 -0800

    added style + connected to api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import logo from './logo.svg';
 import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: [],
+      activeItem: {
+        id: null,
+        title: '',
+        completed: false
+      },
+      editing: false
+    }
+    this.fetchTasks = this.fetchTasks.bind(this)
+  }
+
+  componentWillMount() {
+    this.fetchTasks()
+
+  }
+
+  fetchTasks() {
+    console.log("Fetching ...")
+    fetch('http://127.0.0.1:8000/api/task-list/')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div className="container">
