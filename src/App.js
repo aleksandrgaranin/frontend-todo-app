57c51f4398e5eb9b592334c68e4c86d164956288
aleksandGG <aleksandrgaranin89@gmail.com>
2021-01-13 22:50:35 -0800

    First commit App + styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div id="task-container">
+          <div id="form-wrapper">
+            <form if="form">
+              <div className="flex-wrapper">
+                <div style={{ flex: 6 }}>
+                  <input className="form-control" id="title" type="text" name="title" placeholder="Add task"></input>
+                </div>
+                <div style={{ flex: 1 }}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="Add"></input>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div id="list-wrapper">
+
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
